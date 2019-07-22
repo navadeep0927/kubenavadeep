3c6fb420123a5e77362af1ff26af74e495994c67
Sandeep Reddy Moola <Moola@Sandeeps-MacBook-Pro.local>
2019-07-22 12:09:12 -0500

    Commit File

---
+++ app.py
@@ -0,0 +1,8 @@
+content = {}
+content['fname'] = "navadeep"
+content['lname'] = "moola"
+
+print(content)
+
+print("\n\nFirst Name of User: "+content['fname'])
+print("\n\nLast Name of User: "+content['lname'])
